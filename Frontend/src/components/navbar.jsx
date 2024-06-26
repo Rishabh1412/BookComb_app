2784cc8f11ee9e1f82354724a729b4c4126e51b4
Rish <rishabhkartik2004@gmail.com>
2024-06-26 15:35:57 +0530

    menu section

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -29,9 +29,9 @@ function Navbar() {
 
   const navItems = (
     <>
-      <li><a href="/" className='text-white hover:bg-yellow-400 hover:text-black'>Home</a></li>
-      <li><a href="/course" className='text-white hover:bg-yellow-400 hover:text-black'>Books</a></li>
-      <li><a href="/about" className='text-white hover:bg-yellow-400 hover:text-black'>About</a></li>
+      <li><a href="/">Home</a></li>
+      <li><a href="/course">Books</a></li>
+      <li><a href="/about">About</a></li>
     </>
   );
 
@@ -45,7 +45,9 @@ function Navbar() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
               </div>
               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                {navItems}
+                <li><a href="/" className='text-white hover:bg-yellow-400 hover:text-black'>Home</a></li>
+                <li><a href="/course" className='text-white hover:bg-yellow-400 hover:text-black'>Books</a></li>
+                <li><a href="/about" className='text-white hover:bg-yellow-400 hover:text-black'>About</a></li>
               </ul>
             </div>
             <a className="text-2xl font-bold text-yellow-300">BookComb</a>
