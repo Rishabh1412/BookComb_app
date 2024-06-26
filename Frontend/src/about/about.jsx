fb4e2544929ebfbc54b07d42f60e268893374796
Rish <rishabhkartik2004@gmail.com>
2024-06-26 15:21:39 +0530

    a bit of changes in about section

--- Frontend/src/about/about.jsx
+++ Frontend/src/about/about.jsx
@@ -6,9 +6,14 @@ function about() {
     <>
 
         <Navbar/>
-        <div className=' h-screen justify-center md:pt-24 pt-12 flex'>
+        <div className=' h-screen '>
+          <div className='justify-center md:pt-24 pt-12 flex'>
             <h1 className=' font-extrabold text-4xl text-white'>About Us</h1>
+          </div>
+          <p className='p-7 px-24'>Welcome to BookComb, a web application designed to make discovering and exploring books easier and more enjoyable. Whether you're a casual reader or a book enthusiast, BookComb offers a user-friendly interface to browse, search, and find detailed information about a wide range of books. Dive into the world of literature with ease and find your next great read with BookComb.</p>
+
         </div>
+
       
     </>
   )
